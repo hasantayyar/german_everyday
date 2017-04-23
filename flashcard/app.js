46de2d1fce3be09330a062b150fababe4ec8c5dc
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-04-23 22:55:56 +0200

    flashcard app updated to be dynamic

--- flashcard/app.js
+++ flashcard/app.js
@@ -1,22 +1,33 @@
 const loadJSON = (file, callback) => {
-    let xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', file, true);
-    console.log('Getting data...');
-    xobj.onreadystatechange = () => {
-        if (xobj.readyState === 4 && xobj.status === 200) {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
+  let xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', file, true);
+  console.log('Getting data...');
+  xobj.onreadystatechange = () => {
+    if (xobj.readyState === 4 && xobj.status === 200) {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
 }
 
+const render = (tpl, options) => tpl.replace(
+  /(\{\{)(.*?)(\}\})/g,
+   (match,_1,_2,_3) => options[_2]
+);
+
+let tpl = '<article class="flashcard">';
+tpl +='<input id="flashcard-1" type="checkbox" />';
+tpl +='<label for="flashcard-1">';
+tpl +='<section class="front">{{word}}</section>';
+tpl +='<section class="back">{{description}}</section></label></article>';
 const init = () => {
-    console.log('Init app');
-    loadJSON( "/words.json", response => {
-        console.log('data fetched');
-        let data = JSON.parse(response);
-    });
+  console.log('Init app');
+  loadJSON( "/words.json", response => {
+    console.log('data fetched');
+    let data = JSON.parse(response);
+    $('body').append(render(tpl, data[0]))
+  });
 }
 
 init();
