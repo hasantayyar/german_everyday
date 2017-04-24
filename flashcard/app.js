207be794b84fcc004429c316242d2dcb7bea1bf2
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-04-24 22:52:56 +0200

    old browser compability

--- flashcard/app.js
+++ flashcard/app.js
@@ -1,5 +1,8 @@
-const loadJSON = function(file, callback) {
-  let xobj = new XMLHttpRequest();
+var index = 0;
+var words = [];
+
+var loadJSON = function(file, callback) {
+  var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open('GET', file, true);
   console.log('Getting data...');
@@ -11,26 +14,23 @@ const loadJSON = function(file, callback) {
   xobj.send(null);
 }
 
-let index = 0;
-let words = [];
-
-const refresh = function(){
+var refresh = function(){
   document.getElementById('flashcard').remove();
   document.body.innerHTML += words[index];
 }
 
-const next = function() {
+var next = function() {
   if (index === (words.length - 1)) return;
   index++;
   refresh();
 }
-const prev = function() {
+var prev = function() {
   if(index === 0) return;
   index--;
   refresh();
 }
 
-let tpl = '<article id="flashcard">';
+var tpl = '<article id="flashcard">';
 tpl +='<input id="flashcard-1" type="checkbox" />';
 tpl +='<label for="flashcard-1">';
 tpl +='<section class="front">{{word}}</section>';
@@ -39,21 +39,20 @@ tpl +='</label>';
 tpl +='<div class="nav"><a onclick="prev()">&#x2039; prev</a><a onclick="next()">next &#x203A;</a></div>'
 tpl +='</article>';
 
-const render = function(options) {
+var render = function(options) {
   return tpl.replace(
   /(\{\{)(.*?)(\}\})/g,
    (match,_1,_2,_3) => options[_2]
   );
 }
-const init = function(file) {
+var init = function(file) {
   console.log('Init app');
   loadJSON( file, response => {
     console.log('data fetched');
-    let data = JSON.parse(response);
-    for (let i in data) {
+    var data = JSON.parse(response);
+    for (var i in data) {
       words.push(render(data[i])); 
     }
     document.body.innerHTML += words[0];
   });
 }
-
