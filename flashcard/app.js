e0ae8756b744236092d6377fa3afb8fb093e9dc9
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-04-26 22:20:21 +0200

    flashcards improved

--- flashcard/app.js
+++ flashcard/app.js
@@ -1,5 +1,11 @@
 var index = 0;
 var words = [];
+var months = ['April', 'May'];
+
+for (var mi in months) {
+  var m = months[mi];
+  document.getElementById('nav_sections').innerHTML += '<a onclick="loadSection(\'' + m + '/words.json\')" href="#">' + m + '</a>';
+}
 
 var loadJSON = function(file, callback) {
   var xobj = new XMLHttpRequest();
@@ -14,9 +20,22 @@ var loadJSON = function(file, callback) {
   xobj.send(null);
 }
 
-var refresh = function(){
+var loadSection = function(file) {
+  words = [];
+  document.getElementById('container').innerHTML = '';
+  loadJSON(file, function(response) {
+    console.log('data fetched', file);
+    var data = JSON.parse(response);
+    for (var i in data) {
+      words.push(render(data[i]));
+    }
+    document.getElementById('container').innerHTML += words[0];
+  });
+};
+
+var refresh = function() {
   document.getElementById('flashcard').remove();
-  document.body.innerHTML += words[index];
+  document.getElementById('container').innerHTML += words[index];
 }
 
 var next = function() {
@@ -25,34 +44,27 @@ var next = function() {
   refresh();
 }
 var prev = function() {
-  if(index === 0) return;
+  if (index === 0) return;
   index--;
   refresh();
 }
 
 var tpl = '<article id="flashcard">';
-tpl +='<input id="flashcard-1" type="checkbox" />';
-tpl +='<label for="flashcard-1">';
-tpl +='<section class="front">{{word}}</section>';
-tpl +='<section class="back">{{description}}</section>';
-tpl +='</label>';
-tpl +='<div class="nav"><a onclick="prev()">&#x2039; prev</a><a onclick="next()">next &#x203A;</a></div>'
-tpl +='</article>';
+tpl += '<input id="flashcard-1" type="checkbox" />';
+tpl += '<label for="flashcard-1">';
+tpl += '<section class="front">{{word}}</section>';
+tpl += '<section class="back">{{description}}</section>';
+tpl += '</label>';
+tpl += '<div class="nav"><a onclick="prev()">&#x2039; prev</a><a onclick="next()">next &#x203A;</a></div>'
+tpl += '</article>';
 
 var render = function(options) {
   return tpl.replace(
-  /(\{\{)(.*?)(\}\})/g,
-   (match,_1,_2,_3) => options[_2]
+    /(\{\{)(.*?)(\}\})/g,
+    (match, _1, _2, _3) => options[_2]
   );
 }
 var init = function(file) {
   console.log('Init app');
-  loadJSON( file, response => {
-    console.log('data fetched');
-    var data = JSON.parse(response);
-    for (var i in data) {
-      words.push(render(data[i])); 
-    }
-    document.body.innerHTML += words[0];
-  });
+  loadSection('April/words.json');
 }
