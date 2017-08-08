b3f9aa385ac822593bf94abe3e9a837d7909dfd4
Unknown <jeremy.lefebre@gmail.com>
2017-08-08 09:44:47 +0200

    Hasan's added functionality

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var index = 0;
 var words = [];
 var months = ['German', 'Turkish', 'English'];
 
+var random = true;
+
 for (var mi in months) {
   var m = months[mi];
-  document.getElementById('nav_sections').innerHTML += '<a onclick="loadSection(\'../' + m + '/words.json\')" href="#">' + m + '</a>';
+  document.getElementById('nav_sections').innerHTML += '<a class="' + m + '" onclick="loadSection(\'' + m + '\')" href="#">' + m + '</a>';
 }
 
 var loadJSON = function(file, callback) {
@@ -20,13 +22,20 @@ var loadJSON = function(file, callback) {
   xobj.send(null);
 }
 
-var loadSection = function(file) {
+var loadSection = function(lang) {
+  var file = "../" + lang + "/words.json";
+  $('#nav_sections a').removeClass('active');
+  $('#nav_sections a.' + lang +'').addClass('active');
   words = [];
   document.getElementById('container').innerHTML = '';
   loadJSON(file, function(response) {
     console.log('data fetched', file);
     var data = JSON.parse(response);
-    // var data = shuffle(JSON.parse(response));
+    var data = random ? shuffle(JSON.parse(response)) : JSON.parse(response);
+
+    var count = data.length;
+    $('#display .total').html(count)
+    $('#display .index').html(0)
     for (var i in data) {
       words.push(render(data[i]));
     }
@@ -34,22 +43,22 @@ var loadSection = function(file) {
   });
 };
 
-// // array shuffle utility function
-//  // https://github.com/coolaj86/knuth-shuffle
-//  var shuffle function(array) {
-//   var currentIndex = array.length, temporaryValue, randomIndex;
-//   // While there remain elements to shuffle...
-//   while (0 !== currentIndex) {
-//     // Pick a remaining element...
-//     randomIndex = Math.floor(Math.random() * currentIndex);
-//     currentIndex -= 1;
-//     // And swap it with the current element.
-//     temporaryValue = array[currentIndex];
-//     array[currentIndex] = array[randomIndex];
-//      array[randomIndex] = temporaryValue;
-//    }
-//    return array;
-//  }
+// array shuffle utility function
+ // https://github.com/coolaj86/knuth-shuffle
+ var shuffle = function(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+     array[randomIndex] = temporaryValue;
+   }
+   return array;
+ }
 
 var refresh = function() {
   document.getElementById('flashcard').remove();
@@ -70,8 +79,8 @@ var prev = function() {
 var tpl = '<article id="flashcard">';
 tpl += '<input id="flashcard-1" type="checkbox" />';
 tpl += '<label for="flashcard-1">';
-tpl += '<section class="front">{{word}}</section>';
-tpl += '<horse class="front">{{pronounciation}}</horse>';
+tpl += '<section class="front">{{word}}';
+tpl += '<horse class="front">{{pronounciation}}</horse></section>';
 tpl += '<section class="back">{{description}}</section>';
 tpl += '</label>';
 tpl += '<div class="nav"><a id="previous" onclick="prev()">&#x2039; prev</a><a id="next" onclick="next()">next &#x203A;</a></div>'
@@ -85,5 +94,5 @@ var render = function(options) {
 }
 var init = function(file) {
   console.log('Init app');
-  loadSection('../German/words.json');
+  loadSection('German');
 }
